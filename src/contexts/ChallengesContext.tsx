aa87b29ecea87c28b91da78f9f8150b47d0b4f02
Mateus Zilli Polli <mateus_zillip@hotmail.com>
2021-03-11 19:10:17 -0300

    :rocket:

--- src/contexts/ChallengesContext.tsx
+++ src/contexts/ChallengesContext.tsx
@@ -1,4 +1,4 @@
-import { createContext, useState, ReactNode } from 'react';
+import { createContext, useState, ReactNode, useEffect } from 'react';
 import challenges from './challenges.json';
 
 export const ChallengesContext = createContext({} as ChallengesContextData);
@@ -18,6 +18,14 @@ export function ChallengesProvider({ children }: ChallengesProviderProps) {
         const randomChallengeIndex = Math.floor(Math.random() * challenges.length);
         const challenge = challenges[randomChallengeIndex];
         setActiveChallenge(challenge);
+
+        new Audio('/notification.mp3').play();
+
+        if (Notification.permission === 'granted') {
+            new Notification('New challenge 🎉', {
+                body: `Win ${challenge.amount} xp!`
+            })
+        }
     }
 
     function failChallenge() {
@@ -41,6 +49,10 @@ export function ChallengesProvider({ children }: ChallengesProviderProps) {
         setActiveChallenge(null);
         setChallengesCompleted(challengesCompleted + 1);
     }
+
+    useEffect(() => {
+        Notification.requestPermission();
+    }, [])
 
     return (
         <ChallengesContext.Provider 
